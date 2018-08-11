03e487341aaf64bc8e28d0ec75341a4e1df93448
Alexander <info@protectyoursite.ru>
2018-08-11 13:25:08 +0300

    add menu

--- turbo.php
+++ turbo.php
@@ -2,7 +2,7 @@
 header('Content-Type: text/html; charset=utf-8');
 
 /* Config */
-$siteurl="https://site.ru"; // Здесь меняем на свой сайт
+$siteurl = "https://site.ru"; // Здесь меняем на свой сайт
 $title = "My site"; // Название сайта
 $description = "My description"; // Описание сайта
 $email = "mymail@site.ru"; // Автор и почта
@@ -17,7 +17,7 @@ $author = "Name";
 //define('WP_DEBUG',true);
 
 // подгружаем среду WordPress
-require_once( $_SERVER['DOCUMENT_ROOT'] . '/wp-load.php' );
+require_once($_SERVER['DOCUMENT_ROOT'] . '/wp-load.php');
 
 // Глобальные переменные $wp, $wp_query, $wp_the_query не установлены...
 global $wpdb;
@@ -25,42 +25,51 @@ $list = $wpdb->get_results("SELECT `post_content`, `post_title`,`post_date_gmt`,
 							FROM $wpdb->posts
 							WHERE `post_status` IN ('publish', 'inherit') AND `post_type` IN ('page','post')", 'OBJECT');
 
-if(!$list) exit();
+if (!$list) exit();
 
-$xml='<?xml version="1.0" encoding="utf-8"?>
+$xml = '<?xml version="1.0" encoding="utf-8"?>
 <rss
     xmlns:yandex="http://news.yandex.ru"
     xmlns:media="http://search.yahoo.com/mrss/"
     xmlns:turbo="http://turbo.yandex.ru"
     version="2.0">
 	<channel>
-		<title>'.$title.'</title>
-		<description><![CDATA['.$description.']]></description>
-		<link>'.$siteurl.'/</link>
-		<lastBuildDate>'.date(DATE_ATOM).'</lastBuildDate>
+		<title>' . $title . '</title>
+		<description><![CDATA[' . $description . ']]></description>
+		<link>' . $siteurl . '/</link>
+		<lastBuildDate>' . date(DATE_ATOM) . '</lastBuildDate>
 		<language>ru-ru</language>
-		<managingEditor>'.$email.' ('.$author.')</managingEditor>';
-foreach($list as $item) {
-    $xml.='
+		<managingEditor>' . $email . ' (' . $author . ')</managingEditor>';
+foreach ($list as $item) {
+    $xml .= '
 			<item turbo="true">
-			<title>'.htmlspecialchars($item->post_title).'</title>
-			<link>'.get_permalink($item->ID).'</link>
-			<turbo:content><![CDATA['.htmlspecialchars_decode(preg_replace ('/\[.*?\]/','',$item->post_content));
-    $xml.='
+			<title>' . htmlspecialchars($item->post_title) . '</title>
+			<link>' . get_permalink($item->ID) . '</link>
+			<turbo:content><![CDATA[';
+    /* Добавляем меню - берется первое произвольное, для конкретного укажите верное в theme_location
+    Удалите эту строку, если не хотите добавлять меню */
+    $menu = wp_nav_menu(array(
+        'items_wrap' => '%3$s',
+        'container' => '',
+        //'theme_location' => 'primary', // Расскоментировать и заменить на необходимое меню!
+        'echo' => false
+        ) );
+    $menu = strip_tags($menu,'<a>');
+    $xml.='<menu>'.$menu.'</menu>';
+    /* Конец добавления меню */
+    $xml .= htmlspecialchars_decode(preg_replace('/\[.*?\]/', '', $item->post_content));
+    $xml .= '
 			<p>Для просмотра и добавления комментариев посетите полную версию сайта по ссылке ниже!</p>]]></turbo:content>
-			<author>'.$author.'</author>
-			<pubDate>'.$item->post_date_gmt.'</pubDate>
+			<author>' . $author . '</author>
+			<pubDate>' . $item->post_date_gmt . '</pubDate>
 		</item>';
 }
-$xml.='</channel>
+$xml .= '</channel>
 </rss>';
 //echo $xml;
-if (file_put_contents($_SERVER['DOCUMENT_ROOT'].'/turbo.xml', $xml))
-{
+if (file_put_contents($_SERVER['DOCUMENT_ROOT'] . '/turbo.xml', $xml)) {
     echo "XML файл сгенерирован";
-}
-else
-{
+} else {
     echo "Неизвестная ошибка";
 }
 ?>
